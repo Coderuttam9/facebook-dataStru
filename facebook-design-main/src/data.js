0ab0e7b66e282d87395dfd9bf975f4db911333d5
Uttam Roy <roy77uttam@gmail.com>
2023-06-28 11:38:29 +0600

    facebook data structure

---
+++ facebook-design-main/src/data.js
@@ -0,0 +1,144 @@
+const user_post = [
+  {
+    id: 1,
+    content: "They trust each other",
+    photos:
+      "https://scontent.fdac3-2.fna.fbcdn.net/v/t39.30808-6/347419264_590544946542108_2890943072217932735_n.jpg?stp=dst-jpg_p180x540&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeG_NmGCZYMgY091O0Bu1Vo24IgCoghbeDLgiAKiCFt4Mk8i2pmOwrkiXsFHmNxen-pFyL4RxjUqH0Dix900RdxX&_nc_ohc=7NLmTD6ZzwcAX9kw-Aj&_nc_zt=23&_nc_ht=scontent.fdac3-2.fna&oh=00_AfBK2FPUcB0SlSDoFVzMmg6uyowtyppqT890w-b8vGtOLw&oe=649EBA82",
+    like: 200,
+    Comment: 300,
+    authur: {
+      name: "শংকর কুমার রায়",
+      photos:
+        "https://scontent.fdac3-2.fna.fbcdn.net/v/t39.30808-6/318546067_2088155101377192_1470329902633961577_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFcS4kr8Vgn3IvqSokIvEDnXLapXaxcTlxctqldrFxOXErf9NcI9wxpmuPxVpMYtjO2hcLWJpM0YOncgFkSDrtn&_nc_ohc=sFx2sr1-ZwAAX9W5Bum&_nc_zt=23&_nc_ht=scontent.fdac3-2.fna&oh=00_AfAJ7mKdIvcBgkQDtW1v4EgFt9MtuFbXg-kXltjtQ1Advw&oe=64A059FF",
+      time: 1,
+    },
+  },
+  {
+    id: 2,
+    content: `শেষে, সব দেনা-পাওনার খুচরো হিসেবে আমার
+ভাগে কেবল তুমি থেকো, আর কিচ্ছু চাই না! 🖤🌸`,
+    photos:
+      "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/356363740_1410298316427122_1441334444468559886_n.jpg?stp=dst-jpg_s600x600&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeE7YyS0YNNmvH7UVOkkshvaXLZnhCYU0m1ctmeEJhTSbaFp_3QyPaAkinvKO7smkdgW2KTKXLzJNXTwhOogl8kk&_nc_ohc=AffEvVQSF-IAX9-CcKN&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfDqDi5ZIAb5_F1OsNTp0CfVibzaxacoXi35qVor1yNE9A&oe=64A115E9",
+    like: 24,
+    Comment: 2,
+    authur: {
+      name: "অ' প্রা' প্তি'",
+      photos:
+        "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/347807350_166255919475153_940862660095555070_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH83WF3wua_u18N7QCJqspmdo6cPnNim-N2jpw-c2Kb4-v0zyzkTW3gNiCnAqGMgnEb2aFugiTlimkdZlY5L9WN&_nc_ohc=fC4jd1SDDC0AX-REH3q&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDhrpZdUx-V3IrX3ygXOhdVnILgbmXk_ozNIsV1W2MgEw&oe=64A09B5C",
+      time: 10,
+    },
+  },
+  {
+    id: 3,
+    content: `Snapshot from an epic @runningwild Scottish adventure… coming soon! @disneyplus @natgeotv guess the guest I was taking? `,
+    photos:
+      "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/354447641_826762768811877_6330508622757830198_n.jpg?stp=dst-jpg_p526x296&_nc_cat=1&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeG_t1I9-Lz4pS3Jyd7nTBb4sQqTpVEi9-SxCpOlUSL35A8UoCpVbjkO2AIgt33Wv6hOWsmyjSK9v2j1TNpRmdgi&_nc_ohc=XnZXOKj6uYsAX8wVTNo&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfC5P4L28TBIBdPpq118zwmLG6l1YmCXJoFNOT8t98MB2A&oe=649FE1B4",
+    like: 240,
+    Comment: 332,
+    authur: {
+      name: "Bear Grylls'",
+      photos:
+        "https://scontent.fjsr1-2.fna.fbcdn.net/v/t1.6435-1/141477507_258765818944911_6861958721508356355_n.jpg?stp=dst-jpg_p200x200&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeEYOeVrSYGkFjwyIWSsy4yoJLlyKH7L3SYkuXIofsvdJhn_Uu_uyeC4XxoWtk2ShWR-yXhEps95a80wA72nj3Zt&_nc_ohc=9SBj1ArEfxQAX9Xwq3h&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfC1IXLZc4anPGXamj7Pc24Fa5yDS8_G3KT_EbDkOr7ucg&oe=64C32999",
+      time: 12,
+    },
+  },
+  {
+    id: 4,
+    content: `Snapshot from an epic @runningwild Scottish adventure… coming soon! @disneyplus @natgeotv guess the guest I was taking? `,
+    photos:
+      "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/356391394_564730409201121_3726414883090768513_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeER4LMU6tp-CfKNXEbMWNfMx99TcvrBcKbH31Ny-sFwpojAsKS8S8FYaMkcUyKHBjTOYhN4349cYo6ecrJm7jGd&_nc_ohc=zFJMQpWoCiwAX_5SX89&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfBwM6aP9E1vm8OY-uZF03IBjBIBH1WhBUWqmBoST7ys8g&oe=64A01E44",
+    like: 40,
+    Comment: 32,
+    authur: {
+      name: "Net Speed Meter",
+      photos:
+        "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-1/356412737_564716305869198_427875580223337454_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=103&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeFSyThMYSe6moWMu8pcaxb-eBKsd30GhhB4Eqx3fQaGEFuRbbADG2PnxOjcM7avCJgR4eiR9U_gT5m0qbM83VTv&_nc_ohc=Ltpt6nOYI9sAX9h0Yua&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfALDM33XDAxcS65u4BwETrVuKzBc_t40RuILgIKxNEw8w&oe=64A11A7F",
+      time: 1,
+    },
+  },
+  {
+    id: 5,
+    content: `হ্যাঁ, রে।বয়সটা আমাদের আটকে থাকুক এই প্রাণের মাঠে`,
+    photos:
+      "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/349187965_2965886653541639_3825065285109960435_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFEPWJoTC5jGH1fp_CJGWKU1dEQqvw9fZzV0RCq_D19nObsDgY-jbZDue_S1rx2SolctFTWp9Sxle6byYkaR6bp&_nc_ohc=7MiTKejJbkMAX9LPlxu&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfAhuzwHlK3NTtL3y7vyThO3wZrrsq0K-hMoAWh2sPKlEQ&oe=649FFA37",
+    like: 40,
+    Comment: 32,
+    authur: {
+      name: "শংকর কুমার রায়",
+      photos:
+        "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/318546067_2088155101377192_1470329902633961577_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFcS4kr8Vgn3IvqSokIvEDnXLapXaxcTlxctqldrFxOXErf9NcI9wxpmuPxVpMYtjO2hcLWJpM0YOncgFkSDrtn&_nc_ohc=sFx2sr1-ZwAAX_-rQC0&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfAoeRA9o0R0OY6ypg4lIr7Y3DA0z_gZlhrdxyGFP2-OgQ&oe=64A059FF",
+      time: 1,
+    },
+  },
+  {
+    id: 6,
+    content: `আজ  থেকে কারেন্ট এর  ঝামেলা শেষ।।☺️
+জীবনের সবশক্তি দিয়ে সাফল একজন।।`,
+    photos:
+      "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/355261591_2288977457961977_5859821833129423228_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHsWwWXCRvABBfDXnj0tsPJQGds9C9vuS5AZ2z0L2-5LjtwMAol4HCX5LMN0b2qIdvXHYQhedafYxfxp6URgPg1&_nc_ohc=iWjmx1V3gc0AX8loSBA&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfC7ydCqLgdcBFfrw6dNG_RG-MkixLLNK2UUL7ywayuOjQ&oe=649FC666",
+    like: 40,
+    Comment: 32,
+    authur: {
+      name: "Amirul Islam Ovi",
+      photos:
+        "https://scontent.fjsr1-1.fna.fbcdn.net/v/t1.6435-1/121056734_1537893383070392_5479447672564582399_n.jpg?stp=dst-jpg_p100x100&_nc_cat=105&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeGk68MepBMngR0CmddMjB5G8MCjknp92NzwwKOSen3Y3HOagi6HeioviONBBAHXQ80CBO3mUPO6FncM4dScmUic&_nc_ohc=ooZIqwAJ7FwAX83rBpN&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfBdDCYx5fiUNte0VW4iALISWl2ZUlurB8HqKGij-YHWCw&oe=64C32C69",
+      time: 1,
+    },
+  },
+  {
+    id: 7,
+    content: `দিনাজপুরে নাকহীন এক চোখ বিশিষ্ট বাছুরের জন্ম দিলো গাভী!`,
+    photos:
+      "https://external.fjsr1-2.fna.fbcdn.net/emg1/v/t13/7342345420132878149?url=https%3A%2F%2Fcricde.com%2Fwp-content%2Fuploads%2Fter-1.jpg&fb_obo=1&utld=cricde.com&stp=c0.5000x0.5000f_dst-jpg_flffffff_p500x261_q75&_nc_eui2=AeHzbQfDQpGrUq1oQWAcmV4pHVeDtLxk83gdV4O0vGTzeAr8innsgohTW8WUqdlNSL0tiRDBOjbF0IcWYPhelSYC&ccb=13-1&oh=06_AbG_2R23qEEzla8My_PUbBhOkfdk8uDZIGfsYLbt0J411Q&oe=649DB463&_nc_sid=8afd86",
+    like: 0,
+    Comment: 12,
+    authur: {
+      name: "Bangla post",
+      photos:
+        "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-1/323056383_845958206637937_3912042350586868778_n.jpg?stp=dst-jpg_p100x100&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeFW7cEZJi2VWNajesFaGQjXOk94uk6Wt-46T3i6Tpa37kLqzlBCzaw79M3M0y3gBKB9w7JGpCD2Ypijc8gZgbZe&_nc_ohc=FehtJsaMUDIAX9bpdSA&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfCXCeCblSsWTcE4qYxDSF0WLme1GPyelws_bdWgir_M_Q&oe=64A11A35",
+      time: 1,
+    },
+  },
+  {
+    id: 8,
+    content: `বলিউডে সালমান খানই যথাসম্ভব একমাত্র অভিনেতা যার অনস্ক্রিন কোন কিসিং সিন বা ১৮+ সিন নেই।সালমান খান একবার এক ইন্টারভিউতে বলেন,`,
+    photos:
+      "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/356059296_1000197781335611_1078631357546515475_n.jpg?stp=dst-jpg_p526x296&_nc_cat=1&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFTFMOzPyALLgr9T-3Trp_eN1TMK0pEv503VMwrSkS_nbMlQqnP-mies2sfMQzBSIpgycvNl5B1fl1TVpe9-u9X&_nc_ohc=yeo8shcvv9IAX-pN2EN&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfC2gzRK1rJgn4h7P1ge1uI1nlmuSf5SZfOip3i5f-rUSg&oe=649FC1F7",
+    like: 0,
+    Comment: 11,
+    authur: {
+      name: "ট্রল সালমান খান হেটার্স",
+      photos:
+        "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-1/353625668_994036025285120_4169600217655669929_n.jpg?stp=dst-jpg_p100x100&_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFvsCi2BOjIt59EvftGlTb7XcwkwQpLZ35dzCTBCktnfmo9s9QY1Qns7n3I3MMetTSOkIbsajGs2wib5diLIh8A&_nc_ohc=ibOeHWogFR4AX-viD61&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfDOQalFpiu3ctTA9Wjv3h5C1yqikw1gyKr-ZDR8X1QjWQ&oe=64A175C9",
+      time: 1,
+    },
+  },
+  {
+    id: 9,
+    content: `রাজশাহী থেকে মুরগি কিনছিলাম,,,,এখন ডিম দিছে,,,,,আমের মতো,`,
+    photos:
+      "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/356874675_3516322571973262_7646064282944644945_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeE6qgvm_5r6rg0Gtk7LiQh4D135rcvKoa8PXfmty8qhryfR8rLRKZvScvWMozaeGpo_fWdbzSe825tZmyyTdEsd&_nc_ohc=zmWDK2k0nbgAX-kN1GS&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfCWCbO9PZbks2MACpBaanvV_fh1eQXqEdruDNUHKuwWKw&oe=64A01E74",
+    like: 100,
+    Comment: 3,
+    authur: {
+      name: "ঝরা পাতা",
+      photos:
+        "https://scontent.fjsr1-1.fna.fbcdn.net/v/t1.6435-1/183920711_2933007460304779_4536469333408868046_n.jpg?stp=dst-jpg_p100x100&_nc_cat=109&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeEzM_O-vc-es_Pbsydh9zbCjZGQyZSKHLuNkZDJlIocuzWwfhUowjpTDiM4QhT7hahbkwOHL0IIdRNxem6Atjkt&_nc_ohc=JLfJv5Z27W0AX88Rjq5&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfBv7dq56pD3lqgGBhPMrv47w1KjU0cZZ4mX3P5Mr3zRaQ&oe=64C33266",
+      time: 1,
+    },
+  },
+  {
+    id: 10,
+    content: `এক্সপেকটেশন শুধু আল্লাহর কাছেই রাখতে হয়, মানুষের কাছে রাখা নি'র্ঘা'ত বো'কা'মি!'🌸`,
+    photos:
+      "https://scontent.fjsr1-2.fna.fbcdn.net/v/t39.30808-6/356660751_1410957236361230_7142704777566719199_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeE3-0RUMuoIn7lHdHRickXzVc8XYhkdAwhVzxdiGR0DCA-o6FC_mAD6DFKEvZVr9bzUWrNPNh0BUeNkf4f3ZpIv&_nc_ohc=TVXxiuD4FdoAX_KaSqo&_nc_ht=scontent.fjsr1-2.fna&oh=00_AfCYNJtxFc3KsH4_0UG3Nv0N5A-gDuR1o2oTalfRZCDo3A&oe=64A16067",
+    like: 102,
+    Comment: 31,
+    authur: {
+      name: "অ' প্রা' প্তি'",
+      photos:
+        "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/347807350_166255919475153_940862660095555070_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH83WF3wua_u18N7QCJqspmdo6cPnNim-N2jpw-c2Kb4-v0zyzkTW3gNiCnAqGMgnEb2aFugiTlimkdZlY5L9WN&_nc_ohc=fC4jd1SDDC0AX-REH3q&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDhrpZdUx-V3IrX3ygXOhdVnILgbmXk_ozNIsV1W2MgEw&oe=64A09B5C",
+      time: 1,
+    },
+  },
+];
